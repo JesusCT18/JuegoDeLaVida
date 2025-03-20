f00c62f761a582b883701a01780c711073cde941
eChrls <ecom.jct@gmail.com>
2025-03-20 10:45:59 +0100

    ClaseGeneracion

--- Celula.java
+++ Celula.java
@@ -8,49 +8,17 @@ columna
 boolean viva muerta
  */
 public class Celula {
-    private int fila;
-    private int columna;
-    private boolean viva;
 
-    public Celula(int fila, int columna, boolean viva) {
-        this.fila = fila;
-        this.columna = columna;
-        this.viva = viva;
+    private boolean estado;
+
+    public boolean isEstado() {
+        return estado;
     }
 
-    public int getFila() {
-        return fila;
+    public void setEstado(boolean estado) {
+        this.estado = estado;
     }
 
-    public void setFila(int fila) {
-        this.fila = fila;
-    }
-
-    public int getColumna() {
-        return columna;
-    }
-
-    public void setColumna(int columna) {
-        this.columna = columna;
-    }
-
-    public boolean isViva() {
-        return viva;
-    }
-
-    public void setViva(boolean viva) {
-        this.viva = viva;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Celula{");
-        sb.append("fila=").append(fila);
-        sb.append(", columna=").append(columna);
-        sb.append(", viva=").append(viva);
-        sb.append('}');
-        return sb.toString();
-    }
+    
 
 }
--- Generacion.java
+++ Generacion.java
@@ -1,15 +1,48 @@
 
 package JuegoDeLaVida;
 
+import java.util.Arrays;
+
 /**
-[][]celulas (matriz de posiciones) 
-crearGeneracionAleatoria()
-crearGeneracion%()
-mostrarMatriz()
-mostrarRegistro
-mostrarMenu
-generacionesViva
+ * [][]celulas (matriz de posiciones)
+ * crearGeneracionAleatoria()
+ * crearGeneracion%()
+ * mostrarMatriz()
+ * mostrarRegistro
+ * mostrarMenu
+ * generacionesViva
  */
 public class Generacion {
-    
+
+    private int[][] celulas;
+    private int generacionesViva;
+    private int fila;
+    private int columna;
+
+    public Generacion(int fila, int columna) {
+        this.celulas = new int[fila][columna];
+        this.generacionesViva = 0; // o 1?
+    }
+
+    public int[][] getCelulas() {
+        return celulas;
+    }
+
+    public void setCelulas(int[][] celulas) {
+        this.celulas = celulas;
+    }
+
+    public int getGeneracionesViva() {
+        return generacionesViva;
+    }
+
+    public void setGeneracionesViva(int generacionesViva) {
+        this.generacionesViva = generacionesViva;
+    }
+
+    @Override
+    public String toString() {
+        return "Generacion [celulas=" + Arrays.toString(celulas)
+                + ", generacionesViva=" + generacionesViva + "]";
+    }
 }
