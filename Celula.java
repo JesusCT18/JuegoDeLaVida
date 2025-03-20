8141ffbbc172216d24d8dde1c27129eaa3458c5e
eCharls <ecom.jct@gmail.com>
2025-03-20 10:55:56 +0100

    pruebaCelula

--- Celula.java
+++ Celula.java
@@ -3,21 +3,29 @@ package JuegoDeLaVida;
 
 /**
 CÉLULA (posicion)
-fila
-columna
 boolean viva muerta
  */
 public class Celula {
 
-    private boolean estado;
+    private boolean viva;
 
-    public boolean isEstado() {
-        return estado;
+    public boolean isViva() {
+        return viva;
     }
 
-    public void setEstado(boolean estado) {
-        this.estado = estado;
+    public void setViva(boolean viva) {
+        this.viva = viva;
     }
+
+    public Celula(boolean viva) {
+        this.viva = viva;
+    }
+
+    @Override
+    public String toString() {
+        return "Celula [viva=" + viva + ", isViva()=" + isViva() + "]";
+    }
+
 
     
     
